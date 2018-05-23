cd4f2488540af70d6d449b4d9d26bb41ccb176ed
1 <ppark9553@gmail.com>
2018-05-23 11:17:10 +0900

    commiting lazily: minor change only

--- arbiter/urls.py
+++ arbiter/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from .views import HomeView
@@ -10,3 +12,7 @@ urlpatterns = [
     url(r'^stock-api/', include('stockapi.urls', namespace='stockapi')),
     url(r'^hidden-api/', include('gateway.urls', namespace='hiddenapi')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
