a66ba1d6d1542a9ef6093868da698c6b26976cab
1 <ppark9553@gmail.com>
2018-05-29 13:52:49 +0900

    commiting lazily: minor change only

--- scripts/deploy_redis.sh
+++ scripts/deploy_redis.sh
@@ -5,11 +5,6 @@
 sudo apt-get install redis-server
 rm /etc/redis/redis.conf # remove the original redis.conf file
 sudo cp /home/arbiter/buzzz/config/redis/redis.conf /etc/redis/redis.conf # replace the redis.conf file
-sudo mkdir -p /data/redisdb
-sudo chown -R redis:redis /data
-### CONFIG SET dir /data/redisdb
-### CONFIG SET dbfilename redisdb.rdb
-### BGSAVE
-sudo systemctl restart redis-server
+sudo ufw allow 6379 # open port 6379 for remote access
 
-sudo ufw allow 6379
+sudo systemctl restart redis-server
