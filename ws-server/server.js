698d7e8978f128f00262a4108abed23a06a29b17
1 <ppark9553@gmail.com>
2018-05-29 19:41:37 +0900

    commiting lazily: minor change only

--- ws-server/server.js
+++ ws-server/server.js
@@ -1,17 +1,17 @@
-var io = require('socket.io').listen(3000)
-var pg = require ('pg')
+const io = require('socket.io').listen(3000)
+const pg = require ('pg')
 
-var con_string = 'tcp://arbiter:makeitpopweAR!1@45.32.59.138/arbiter'
+const con_string = 'tcp://arbiter:makeitpopweAR!1@445.77.134.175/arbiter'
 
-var pg_client = new pg.Client(con_string)
+let pg_client = new pg.Client(con_string)
 pg_client.connect()
-var query = pg_client.query('LISTEN "projectstate"')
+let query = pg_client.query('LISTEN "projectstate"')
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', socket => {
     socket.emit('connected', { connected: true })
 
-    socket.on('ready for data', function (data) {
-        pg_client.on('notification', function(id) {
+    socket.on('ready for data', data => {
+        pg_client.on('notification', id => {
             socket.emit('update', { message: id })
         })
     })
